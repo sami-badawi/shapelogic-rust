785fd6cdf735bede18761e02aac09d08c18d1b63
Sami Badawi <sami.badawi@gmail.com>
2017-10-11 20:14:44 -0400

    First small refactor

---
+++ src/io_helper.rs
@@ -0,0 +1,20 @@
+extern crate image;
+
+use std;
+use std::fs::File;
+use std::path::Path;
+
+use self::image::GenericImage;
+
+/// Function to test that image can open and save
+pub fn jpg_to_png(filename: std::string::String) {
+let im = image::open(&Path::new(&filename)).unwrap();
+
+println!("dimensions {:?}", im.dimensions());
+
+println!("{:?}", im.color());
+
+let fout = &mut File::create(&Path::new(&format!("{}.png", filename))).unwrap();
+
+im.save(fout, image::PNG).unwrap();
+}
