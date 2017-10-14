b993927e3d7d170bd6a09af1ca8c75aaeef36baf
Sami Badawi <sami.badawi@gmail.com>
2017-10-13 20:57:12 -0400

    Now you can get both input and output filenames

--- src/io_helper.rs
+++ src/io_helper.rs
@@ -7,17 +7,19 @@ use std::path::Path;
 use self::image::GenericImage;
 
 /// Convert jpeg image to png image
-pub fn jpg_2_png(
-    input_filename: std::string::String,
-    output_filename: std::string::String
-) {
+pub fn jpg_2_png(input_filename: std::string::String, output_filename: std::string::String) {
     let im = image::open(&Path::new(&input_filename)).unwrap();
 
     println!("dimensions {:?}", im.dimensions());
 
     println!("{:?}", im.color());
 
-    let fout = &mut File::create(&Path::new(&output_filename)).unwrap();
+    let output_filename_with_ending = if output_filename.ends_with(".png") {
+        output_filename
+    } else {
+        format!("{}.png", output_filename)
+    };
+    let fout = &mut File::create(&Path::new(&output_filename_with_ending)).unwrap();
 
     im.save(fout, image::PNG).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 /// ShapeLogic Rust main
-use std::env;
+extern crate clap;
+
+use clap::{App, Arg};
 mod io_helper;
 
 pub fn main() {
+    let matches = App::new("ShapeLogic Rust")
+        .version("0.1.0")
+        .author("Hackerman Jones <hckrmnjones@hack.gov>")
+        .about("Teaches argument parsing")
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .takes_value(true)
+                .help("Input file name"),
+        )
+        .arg(
+            Arg::with_name("out")
+                .short("o")
+                .long("out")
+                .takes_value(true)
+                .help("Output file name"),
+        )
+        .get_matches();
+
+
+    let filename = matches
+        .value_of("file")
+        .unwrap_or("./img/Lenna.jpg")
+        .to_string();
+    let output = matches.value_of("out").unwrap_or("").to_string();
+    println!("Input file: {}; Output fiel: {}", filename, output);
+
     println!("Yes I know what you are thinking. More languages, more vaporware. :D");
-    let filename = if env::args().count() == 2 {
-        env::args().nth(1).unwrap()
-    } else {
-        "./img/Lenna.jpg".to_owned()
-    };
     println!("{}", filename);
-    io_helper::jpg_to_png(filename);
+    if output.is_empty() {
+        io_helper::jpg_to_png(filename);
+    } else {
+        io_helper::jpg_2_png(filename, output);
+    }
 }
