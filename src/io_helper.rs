ac6c4b729cfa455581c5637b99d41faee926b4a2
Sami Badawi <sami.badawi@gmail.com>
2017-10-13 22:20:28 -0400

    Work on returning different image formats. Hit first 100 lines of code

--- src/io_helper.rs
+++ src/io_helper.rs
@@ -5,6 +5,40 @@ use std::fs::File;
 use std::path::Path;
 
 use self::image::GenericImage;
+use self::image::ImageFormat;
+
+/// Should be made automatic
+#[allow(dead_code)]
+pub fn extension_2_enum(extension: &str) -> ImageFormat {
+    let extension_string = extension.to_owned();
+    let extension_lower = extension_string.to_lowercase();
+    match extension_lower.as_str() {
+        "png" => ImageFormat::PNG,
+        "jpg" | "jpeg" => ImageFormat::JPEG,
+        "tiff" => ImageFormat::TIFF,
+        _ => ImageFormat::PNG,
+    }
+}
+
+/// General way to get from input name, given output name and extension to real output name
+#[allow(dead_code)]
+pub fn input_to_output_name(
+    input_filename: &str,
+    output: &str,
+    extension: &str,
+) -> std::string::String {
+    let output_filename: &str = if output.is_empty() {
+        input_filename
+    } else {
+        output
+    };
+    let output_filename_with_ending: String = if output_filename.ends_with(extension) {
+        output_filename.to_owned()
+    } else {
+        format!("{}.{}", output_filename, extension)
+    };
+    output_filename_with_ending
+}
 
 /// Convert jpeg image to png image
 pub fn jpg_2_png(input_filename: std::string::String, output_filename: std::string::String) {
