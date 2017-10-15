7bb3f65621d134886efb4171f475df81876c3c55
Sami Badawi <sami.badawi@gmail.com>
2017-10-15 19:47:55 -0400

    Threshold operation

--- src/image_operations.rs
+++ src/image_operations.rs
@@ -1,6 +1,18 @@
 /// Smaller image processing operations
 extern crate image;
 
-pub fn threshold(img: image::DynamicImage, _limit: u8) -> image::DynamicImage {
-    img
+use self::image::{DynamicImage, GenericImage, Pixel};
+
+pub fn threshold(img: DynamicImage, limit: u8) -> DynamicImage {
+    let mut noisy = img.brighten(-25);
+    let height = img.height();
+    let width = img.width();
+    for x in 0..(width) {
+        for y in 0..(height) {
+            let px = img.get_pixel(x, y)
+                .map(|v| if v <= limit { 0 } else { 255 });
+            noisy.put_pixel(x, y, px);
+        }
+    }
+    noisy
 }
