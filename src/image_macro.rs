7b5658e3c09014b01a65a88d0a2c786f44e16952
Sami Badawi <sami.badawi@gmail.com>
2017-10-16 03:35:57 -0400

    Start work on macro language

---
+++ src/image_macro.rs
@@ -0,0 +1,10 @@
+/// To handle a sequence of image operations
+/// 
+/// It will parse input file in this format:
+/// load img/text.png
+/// edge
+/// threshold 40
+/// invert
+/// save img/lines.png
+
+
