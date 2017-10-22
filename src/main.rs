22636336522d0b16141804f47b790bb3c7090ab9
Sami Badawi <sami.badawi@gmail.com>
2017-10-22 19:57:52 -0400

    Prepare version 0.3.0

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod model_collection;
 
 pub fn main() {
     let matches = App::new("ShapeLogic Rust")
-        .version("0.2.0")
+        .version("0.3.0")
         .author("Sami Badawi")
         .about("ShapeLogic Rust, computer vision and image processing in Rust. Example: 
 shapelogic-rust --file img/Lenna.jpg --out img/output -e png -t edge
