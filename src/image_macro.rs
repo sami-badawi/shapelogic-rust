e8ce4a1b7630e02edb65a59909390cd4d1dbe744
Sami Badawi <sami.badawi@gmail.com>
2017-10-22 12:51:05 -0400

    Test that full macro parser is working

--- src/image_macro.rs
+++ src/image_macro.rs
@@ -15,7 +15,8 @@ pub struct ImageCommand<'a> {
 
 // --------------------- parse_one ---------------------
 
-pub fn parse_one(macro_input: &str) -> ImageCommand {
+pub fn parse_one(macro_input_raw: &str) -> ImageCommand {
+    let macro_input = macro_input_raw.trim();
     let index_pt = macro_input.find(' ');
     let string_len = macro_input.len();
     let res = match index_pt {
@@ -80,12 +81,45 @@ fn parse_all_1_test() {
 }
 
 #[test]
-fn parse_all_2_test() {
+fn parse_all_2_1_test() {
     let expected = ImageCommand {
         command: "threshold",
         parameter: "127",
     };
     let result_found = parse_all("threshold 127");
     assert_eq!(expected, result_found[0]);
-    assert_eq!(1, result_found.len())
+    assert_eq!(1, result_found.len());
+    assert_eq!(vec![expected], result_found)
+}
+
+#[test]
+fn parse_all_2_2_test() {
+    let expected = ImageCommand {
+        command: "threshold",
+        parameter: "127",
+    };
+    let expected2 = ImageCommand {
+        command: "gray",
+        parameter: "",
+    };
+    let result_found = parse_all("threshold 127; gray");
+    assert_eq!(expected, result_found[0]);
+    assert_eq!(2, result_found.len());
+    assert_eq!(vec![expected, expected2], result_found)
+}
+
+#[test]
+fn parse_all_2_3_test() {
+    let expected = ImageCommand {
+        command: "threshold",
+        parameter: "127",
+    };
+    let expected2 = ImageCommand {
+        command: "gray",
+        parameter: "",
+    };
+    let result_found = parse_all("threshold 127; gray;");
+    assert_eq!(expected, result_found[0]);
+    assert_eq!(2, result_found.len());
+    assert_eq!(vec![expected, expected2], result_found)
 }
