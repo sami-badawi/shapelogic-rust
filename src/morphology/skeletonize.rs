cf1336cffb4c9dcb88c88629a74730605c7bc552
Sami Badawi <sami.badawi@gmail.com>
2017-10-26 23:11:11 -0400

    Got value out of buffer

--- src/morphology/skeletonize.rs
+++ src/morphology/skeletonize.rs
@@ -25,7 +25,11 @@ pub fn make_gray(imgx: u32, imgy: u32) -> GrayImage {
 fn make_gray_test() {
     let new_gray = make_gray(4, 4);
     let actual = new_gray.dimensions();
-    assert_eq!((4, 4), actual)
+    assert_eq!((4, 4), actual);
+
+    // let pixel1 = new_gray[(100, 100)];
+    let pixel2 = new_gray.get_pixel(0, 0);
+    assert_eq!(&image::Luma([0u8]), pixel2);
 }
 
 // ---------------------------------------
