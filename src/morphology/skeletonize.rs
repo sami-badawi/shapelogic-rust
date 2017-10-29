7a5f7b926d90ecb2de8faa812c00f1be990cf06f
Sami Badawi <sami.badawi@gmail.com>
2017-10-29 16:08:50 -0400

    Skeletonize close to done

--- src/morphology/skeletonize.rs
+++ src/morphology/skeletonize.rs
@@ -7,10 +7,17 @@ use self::image::{DynamicImage, GrayImage, ImageLuma8};
 use morphology::image_create;
 use morphology::morphology_data::{TABLE2, TABLE};
 
+// ------------------ Constants ---------------------
+
 #[allow(dead_code)]
 const BACKGROUND_COLOR: u8 = 0;
 #[allow(dead_code)]
 const FOREGROUND_COLOR: u8 = 255;
+
+const VERBOSE_LOGGING: bool = true;
+const MAX_PASS: u32 = 100;
+
+// ------------------ Skeletonize ---------------------
 
 ///
 #[allow(dead_code)]
@@ -28,30 +35,30 @@ pub fn skeletonize(image: &GrayImage) -> Option<DynamicImage> {
 
 // ------------------ Helper class not sure if needed ---------------------
 
-
 #[allow(dead_code)]
 pub struct Skeletonize<'a> {
     input_img: &'a GrayImage,
-    input_buffer: Vec<u8>,
+    buffer_even: Vec<u8>,
+    buffer_odd: Vec<u8>,
     inverted: bool,
     find_outline: bool,
     stride: u32,
-    pass: u32,
 }
 
 impl<'a> Skeletonize<'a> {
     #[allow(dead_code)]
     pub fn new(input_image: &'a GrayImage) -> Skeletonize<'a> {
         let width = input_image.width();
-        let raw = input_image.clone().into_raw();
+        let raw0 = input_image.clone().into_raw();
+        let raw1 = input_image.clone().into_raw();
 
         Skeletonize {
             input_img: input_image,
-            input_buffer: raw,
+            buffer_even: raw0,
+            buffer_odd: raw1,
             inverted: false,
             find_outline: true,
             stride: width,
-            pass: 0,
         }
     }
 
@@ -63,8 +70,14 @@ impl<'a> Skeletonize<'a> {
     }
 
     #[allow(dead_code)]
-    pub fn thin(&self, table: &[u32]) -> (Vec<u8>, bool) {
+    pub fn thin(&mut self, pass: u32, table: &[u32]) -> u32 {
         let row_offset = self.stride as usize;
+        let even = pass % 2 == 0;
+        let (in_buffer, output_pixels) = if even {
+            (&mut self.buffer_even, &mut self.buffer_odd)
+        } else {
+            (&mut self.buffer_odd, &mut self.buffer_even)
+        };
 
         let mut p1: u8;
         let mut p2: u8;
@@ -83,14 +96,13 @@ impl<'a> Skeletonize<'a> {
         let mut offset: usize;
 
         let mut pixels_removed: u32 = 0;
-        let in_buffer = &self.input_buffer;
         let bg_color = BACKGROUND_COLOR;
 
-        let mut output_pixels = self.make_buffer();
+
         let (width, height) = self.input_img.dimensions();
         for y in 1..(height - 2) {
             offset = (1 + y as usize * row_offset) as usize;
-            for x in 1..(width - 2) {
+            for _x in 1..(width - 2) {
                 p5 = in_buffer[offset];
                 v = p5;
                 if v != bg_color {
@@ -128,7 +140,7 @@ impl<'a> Skeletonize<'a> {
                         index |= 128u32
                     }
                     code = table[index as usize];
-                    if (self.pass & 1) == 1 {
+                    if (pass & 1) == 1 {
                         //odd pass
                         if code == 2 || code == 3 {
                             v = bg_color;
@@ -146,12 +158,40 @@ impl<'a> Skeletonize<'a> {
                 offset += 1;
             }
         }
-        (output_pixels, 0 < pixels_removed)
+        pixels_removed
     }
 
     #[allow(dead_code)]
-    pub fn skeletonize(image: &GrayImage) -> Option<DynamicImage> {
-        let (width, height) = image.dimensions();
+    pub fn skeletonize(&mut self) -> Option<DynamicImage> {
+
+        let mut pass: u32 = 0;
+        for _ in 0..MAX_PASS {
+            let mut pixels_removed = self.thin(pass, &TABLE);
+            pass += 1;
+            pixels_removed += self.thin(pass, &TABLE);
+            pass += 1;
+            if VERBOSE_LOGGING {
+                println!("table: pass: {}, pixels_removed: {}", pass, pixels_removed);
+            }
+            if pixels_removed <= 0 {
+                break;
+            }
+        }
+        for _ in 0..MAX_PASS {
+            // use a second table to remove "stuck" outputPixels
+            let mut pixels_removed = self.thin(pass, &TABLE2);
+            pass += 1;
+            pixels_removed += self.thin(pass, &TABLE2);
+            pass += 1;
+            if VERBOSE_LOGGING {
+                println!("table: pass: {}, pixels_removed: {}", pass, pixels_removed);
+            }
+            if pixels_removed <= 0 {
+                break;
+            }
+        }
+
+        let (width, height) = self.input_img.dimensions();
         let mut buffer = image_create::make_raw_buffer(width, height);
         buffer[0] = FOREGROUND_COLOR;
         println!("buffer.len() {}", buffer.len());
