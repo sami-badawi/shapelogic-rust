062ec2c8cf20b9034a0e0c4ea5d47bd88e7b6757
Sami Badawi <sami.badawi@gmail.com>
2017-10-29 14:13:12 -0400

    Did a lot of work on thin function

--- src/morphology/skeletonize.rs
+++ src/morphology/skeletonize.rs
@@ -31,6 +31,7 @@ pub fn skeletonize(image: &GrayImage) -> Option<DynamicImage> {
 #[allow(dead_code)]
 pub struct Skeletonize<'a> {
     input_img: &'a GrayImage,
+    input_buffer: Vec<u8>,
     inverted: bool,
     find_outline: bool,
     stride: u32,
@@ -40,9 +41,11 @@ impl<'a> Skeletonize<'a> {
     #[allow(dead_code)]
     pub fn new(input_image: &'a GrayImage) -> Skeletonize<'a> {
         let width = input_image.width();
+        let raw = input_image.clone().into_raw();
 
         Skeletonize {
             input_img: input_image,
+            input_buffer: raw,
             inverted: false,
             find_outline: true,
             stride: width,
@@ -56,9 +59,74 @@ impl<'a> Skeletonize<'a> {
         buffer
     }
 
-    #[allow(dead_code)]
-    pub fn thin(&self, buffer: Vec<u8>) -> Vec<u8> {
-        buffer
+    #[allow(dead_code, )]
+    pub fn thin(&self) -> (Vec<u8>, bool) {
+        let rowOffset = self.stride as usize;
+
+        let mut p1: u8 = 0;
+        let mut p2: u8 = 0;
+        let mut p3: u8 = 0;
+        let mut p4: u8 = 0;
+        let mut p5: u8 = 0;
+        let mut p6: u8 = 0;
+        let mut p7: u8 = 0;
+        let mut p8: u8 = 0;
+        let mut p9: u8 = 0;
+
+
+        let mut v: u8 = 0;
+        let mut index: u32 = 0;
+        let mut code: u32 = 0;
+        let mut offset: usize = 0;
+
+        let mut pixels_removed: u32 = 0;
+        let mut count: u32 = 100;
+        let in_buffer = &self.input_buffer;
+        let bg_color = BACKGROUND_COLOR;
+
+        let mut buffer = self.make_buffer();
+        let (width, height) = self.input_img.dimensions();
+        for y in 1..(height - 2) {
+            for x in 1..(width - 2) {
+                p5 = in_buffer[offset];
+                v = p5;
+                // if v != bg_color {
+                  p1 = in_buffer[offset - rowOffset - 1];
+                  p2 = in_buffer[offset - rowOffset];
+                  p3 = in_buffer[offset - rowOffset + 1];
+                  p4 = in_buffer[offset - 1];
+                  p6 = in_buffer[offset + 1];
+                  p7 = in_buffer[offset + rowOffset - 1];
+                  p8 = in_buffer[offset + rowOffset];
+                  p9 = in_buffer[offset + rowOffset + 1];
+                  index = 0;
+                  if p1 != bg_color {index |= 1u32}
+                  if p2 != bg_color {index |= 2u32}
+                  if p3 != bg_color {index |= 4u32}
+                  if p6 != bg_color {index |= 8u32}
+                  if p9 != bg_color {index |= 16u32}
+                  if p8 != bg_color {index |= 32u32}
+                  if p7 != bg_color {index |= 64u32}
+                  if p4 != bg_color {index |= 128u32}
+                //   code = table(index)
+                //   if ((pass & 1) == 1) { //odd pass
+                //     if (code == 2 || code == 3) {
+                //       v = bg_color
+                //       pixels_removed += 1
+                //     }
+                //   } else { //even pass
+                //     if (code == 1 || code == 3) {
+                //       v = bg_color
+                //       pixels_removed += 1
+                //     }
+                //   }
+                // // }
+                // //         outputPixels(offset) = v
+                // // offset += 1
+            }
+        }
+        buffer[0] = FOREGROUND_COLOR;
+        (buffer, false)
     }
 
     #[allow(dead_code)]
