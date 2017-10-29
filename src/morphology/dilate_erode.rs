1cd1412a5d91446db7bdf7060de8ba9dcea7cf95
Sami Badawi <sami.badawi@gmail.com>
2017-10-29 08:02:28 -0400

    Finished dilate function. Not tested yet

--- src/morphology/dilate_erode.rs
+++ src/morphology/dilate_erode.rs
@@ -4,7 +4,7 @@
 extern crate image;
 
 #[allow(unused_imports)]
-use self::image::{DynamicImage, GenericImage, GrayImage, Luma, Pixel};
+use self::image::{DynamicImage, GenericImage, GrayImage, ImageLuma8, Luma, Pixel};
 use morphology::morphology_data::CYCLE_POINTS_2D;
 
 #[allow(dead_code)]
@@ -21,8 +21,8 @@ pub fn dilate(image: &GrayImage) -> DynamicImage {
     for y in 1..height - 1 {
         for x in 1..width - 1 {
             let mut gray_value: u8 = BACKGROUND_COLOR;
-            for _xy in &CYCLE_POINTS_2D {
-                let p = image.get_pixel(x as u32, y as u32);
+            for xy in &CYCLE_POINTS_2D {
+                let p = image.get_pixel(x + xy.0 as u32, y + xy.1 as u32);
                 if p.data == [FOREGROUND_COLOR] {
                     gray_value = FOREGROUND_COLOR;
                     break;
@@ -33,6 +33,21 @@ pub fn dilate(image: &GrayImage) -> DynamicImage {
     }
     let res = image::ImageLuma8(imgbuf);
     res
+}
+
+#[allow(dead_code)]
+pub fn dilate_dynamic(image: &DynamicImage) -> DynamicImage {
+    match *image {
+        ImageLuma8(ref gray_image) => dilate(gray_image),
+        _ => {
+            let color_type = image.color();
+            println!(
+                "color_type of image not right for dilate: {:?}",
+                &color_type
+            );
+            image.clone()
+        }
+    }
 }
 
 #[allow(dead_code)]
